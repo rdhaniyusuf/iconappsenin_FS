2f46e5b4b58992bcd1d5ee425578da4a6292320d
Rahmadhani Yusuf <52237655+rdhaniyusuf@users.noreply.github.com>
2025-02-02 04:40:56 +0700

    push updated frontend and adding flowchart

--- backend/api/services/user_service.py
+++ backend/api/services/user_service.py
@@ -1,7 +1,5 @@
 from api.models.user_model import User
 from api.auth.auth_service import AuthService
-import jwt
-import datetime
 
 class UserService:
     def create_user(self, username, password):
@@ -15,11 +13,8 @@ class UserService:
         if user and AuthService.check_password(user.password_hash, password):
             return user
         return None
+    
     def generate_jwt(self, user):
-        payload = {
-            'user_id': user.id,
-            'user_name': user.username,
-            'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=1)  # Token expires in 1 hour
-        }
-        token = jwt.encode(payload, 'secureToken#appsenin@2025', algorithm='HS256')
-        return token
+        if user and AuthService.generate_jwt(user):
+            return AuthService.generate_jwt(user)
+        return None
